dba6a060c299a09ab32a1c9ea021843830292bf5
jeevsan1 <93851898+jeevsan1@users.noreply.github.com>
2021-11-07 20:29:26 -0600

    Update symptoms_anxiety_disorder.js

--- pages/symptoms_anxiety_disorder.js
+++ pages/symptoms_anxiety_disorder.js
@@ -9,12 +9,11 @@ const symptoms_anxiety_disorder = () => {
         <head></head>
         <div>
         <div className={Style.anxiety_disorder}>
-            <h1> Here are a list of symptoms that are caused by Anxienty Symptoms:</h1>
+            <h1> Here are a list of symptoms that are split into Physical, Psychological, and Behavioral:</h1>
             <h1> <ul>
-                <li>Generalized Anxiety Disorder (GAD)- Persistent and excessive anxiety and worry about activities or events- even if they are routine or insignificant. This often leads to other disorders and depression </li>
-                <li>Obsessive-Compulsive Disorder (OCD)- Recurrent, unwanted thoughts and the people who have it try to do rituals to get rid of these thoughts but its only temporary </li>
-                <li>Panic Disorder- Repeated episodes of sudden feelings of intense anxiety and fear or terror that reach a peak within minutes panic attacks). You may have feelings of impending doom, shortness of breath, chest pain, or a rapid, fluttering or pounding heart heart palpitations. These panic attacks may lead to worrying about them happening again or avoiding situations in which theyve occurred.</li>              
-                <li>Social Phobia/Anxiety Disorder- High levels of anxiety, fear and avoidance of social situations due to feelings of embarrassment, self-consciousness and concern about being judged or viewed negatively by others.</li>              
+                <li>Psychological: Exessive worrying, Irritability, Impatience, Feeling on edge, fatigue, vivid dreams </li>
+                <li>Physical - Heart Pounding, Flushing, hortness of breath, Dizziness, Sweating, Headache, Dry Mouth</li>              
+                <li>Behavioral - Avoidance of situations, Distress in Social Situations</li>              
                 </ul></h1>
         <Image src="/img(4).jpg" height="1080vh"width="2056vh"alt="Anxiety"></Image>
         </div>
