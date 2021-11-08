6e061f9369427bffff8d6b43b531e5131edfcd5c
jeevsan1 <93851898+jeevsan1@users.noreply.github.com>
2021-11-07 20:39:41 -0600

    Update anxiety_treatment.js

--- pages/anxiety_treatment.js
+++ pages/anxiety_treatment.js
@@ -14,7 +14,8 @@ const anxiety_treatment = () => {
             <h1> Here are a list of treatments:</h1>
             <h1> <ul>
                 <li>Pyscotherapy- Despite the creepy name psycotherapy is just talking to someone generally a counsler to improve one Anxiety disorder</li>
-                <li>Medications- Certain Antidepressants</li>              
+                <li>Medications- Certain Antidepressants</li>    
+                <li>Some ways to manage your Anxiety - Limit your information, Be positive, Excerise, Relaxation, Health Check, Enough Sleep</li>          
                 </ul></h1>
             <h1><a href="pdf.pdf" download className={Style.Link}>Download</a> a free pdf for Coping skills with Anxiety!</h1>
         <Image src="/img(5).jpg" height="1080vh"width="2056vh" alt="Anxiety"></Image>
